5fd90b5b31c8d379bf9e75e544604624ce130f77
lu1zibrahim <luizibrahim94@gmail.com>
2021-11-05 14:54:39 -0300

    End of the review for string slicing

--- Exercicios/exer1.py
+++ Exercicios/exer1.py
@@ -357,10 +357,10 @@
 
 
 
-s1 = input("Digite uma string ")
-while s1 != '':
-    print(s1)
-    s1 = s1[:-1]
+# s1 = input("Digite uma string ")
+# while s1 != '':
+#     print(s1)
+#     s1 = s1[:-1]
 # 11 - Jogo de Forca. Desenvolva um jogo da forca. O programa terá uma lista de palavras lidas de um arquivo texto e escolherá uma aleatoriamente. O jogador poderá errar 6 vezes antes de ser enforcado.
 #
 # Digite uma letra: A
