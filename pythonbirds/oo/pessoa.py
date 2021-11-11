a5128794e8370fde686067460cc410d0599eabbd
lu1zibrahim <luizibrahim94@gmail.com>
2021-11-11 06:40:21 -0300

    Criado Atributo complexo filhos.

--- pythonbirds/oo/pessoa.py
+++ pythonbirds/oo/pessoa.py
@@ -1,17 +1,21 @@
 
 class Pessoa:
-    def __init__(self, nome = None, idade = 27):
+    def __init__(self, *filhos, nome = None, idade = 27):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == "__main__":
-    p = Pessoa('Luiz')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = "Luiz"
-    print(p.nome)
+    luiz = Pessoa(nome='Luiz')
+    carlos = Pessoa(luiz, nome='Carlos')
+    print(Pessoa.cumprimentar(luiz))
+    print(id(luiz))
+    print(luiz.cumprimentar())
+    print(luiz.nome)
+    print(carlos.nome)
+    print(carlos.idade)
+    for filho in carlos.filhos:
+        print(filho.nome)
