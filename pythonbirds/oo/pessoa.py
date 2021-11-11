5933d94ffe7de09a3a89047408948bc2693b28c7
lu1zibrahim <luizibrahim94@gmail.com>
2021-11-11 07:00:04 -0300

    Criado metodos da classe.

--- pythonbirds/oo/pessoa.py
+++ pythonbirds/oo/pessoa.py
@@ -2,7 +2,6 @@
 class Pessoa:
     OLHOS = 2
 
-
     def __init__(self, *filhos, nome = None, idade = 27):
         self.idade = idade
         self.nome = nome
@@ -10,6 +9,14 @@ class Pessoa:
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.OLHOS}'
 
 if __name__ == "__main__":
     luiz = Pessoa(nome='Luiz')
@@ -33,3 +40,5 @@ if __name__ == "__main__":
     print(Pessoa.OLHOS)
     print(luiz.OLHOS)
     print(id(Pessoa.OLHOS), id(luiz.OLHOS), id (carlos.OLHOS))
+    print(Pessoa.metodo_estatico(), luiz.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luiz.nome_e_atributos_de_classe())
