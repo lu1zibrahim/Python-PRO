e4b285ebaa645ff042eca4d35fb66e14ac6fdd9d
lu1zibrahim <luizibrahim94@gmail.com>
2021-11-11 06:53:41 -0300

    Criado atributo de Classe OLHOS.

--- pythonbirds/oo/pessoa.py
+++ pythonbirds/oo/pessoa.py
@@ -1,5 +1,8 @@
 
 class Pessoa:
+    OLHOS = 2
+
+
     def __init__(self, *filhos, nome = None, idade = 27):
         self.idade = idade
         self.nome = nome
@@ -19,8 +22,14 @@ if __name__ == "__main__":
     print(carlos.idade)
     for filho in carlos.filhos:
         print(filho.nome)
+    luiz.OLHOS = 1
+    del luiz.OLHOS
     carlos.sobrenome = "Ibrahim"
     print(carlos.sobrenome)
     del carlos.filhos
     print(carlos.__dict__)
     print(luiz.__dict__)
+    Pessoa.OLHOS = 3
+    print(Pessoa.OLHOS)
+    print(luiz.OLHOS)
+    print(id(Pessoa.OLHOS), id(luiz.OLHOS), id (carlos.OLHOS))
