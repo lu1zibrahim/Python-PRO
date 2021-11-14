a6df1713abaf5efade1eb7f7e61aaf9dfe67b75c
lu1zibrahim <luizibrahim94@gmail.com>
2021-11-13 21:04:54 -0300

    End of the tests

--- pytools/libpythonpro/libpythonpro/tests/test_spam/test_github_api.py
+++ pytools/libpythonpro/libpythonpro/tests/test_spam/test_github_api.py
@@ -6,7 +6,7 @@ from pytools.libpythonpro.libpythonpro import github_api
 
 
 @pytest.fixture()
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/61382982?v=4'
     resp_mock.json.return_value = {
@@ -15,10 +15,9 @@ def avatar_url():
         'node_id': 'MDQ6VXNlcjYxMzgyOTgy',
         'avatar_url': url,
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('pytools.libpythonpro.libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
